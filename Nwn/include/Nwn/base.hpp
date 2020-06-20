8dff5581125f96be211f8a1228ee3977e11e35a7
Raihan Kibria <rkibria@users.noreply.github.com>
2020-06-20 02:14:41 +0100

    Alignment Bimap

--- Nwn/include/Nwn/base.hpp
+++ Nwn/include/Nwn/base.hpp
@@ -1,6 +1,9 @@
 #ifndef NWNBASE_H
 #define NWNBASE_H
 
+#include <boost/bimap.hpp>
+#include <boost/assign.hpp>
+
 namespace Nwn {
 
 enum class AblScore : int {
@@ -11,6 +14,32 @@ enum class AblScore : int {
     Wis,
     Cha
 };
+
+enum class Alignment : int {
+    LawfulGood,
+    NeutralGood,
+    ChaoticGood,
+    LawfulNeutral,
+    Neutral,
+    ChaoticNeutral,
+    LawfulEvil,
+    NeutralEvil,
+    ChaoticEvil
+};
+
+using AlignmentBimap = boost::bimap< Alignment, std::string >;
+
+static const AlignmentBimap alignmentStrings = boost::assign::list_of< AlignmentBimap::relation >
+    ( Alignment::LawfulGood, "Lawful Good" )
+    ( Alignment::NeutralGood, "Neutral Good" )
+    ( Alignment::ChaoticGood, "Chaotic Good" )
+    ( Alignment::LawfulNeutral, "Lawful Neutral" )
+    ( Alignment::Neutral, "Neutral" )
+    ( Alignment::ChaoticNeutral, "Chaotic Neutral" )
+    ( Alignment::LawfulEvil, "Lawful Evil" )
+    ( Alignment::NeutralEvil, "Neutral Evil" )
+    ( Alignment::ChaoticEvil, "Chaotic Evil" )
+    ;
 
 } // namespace Nwn
 
