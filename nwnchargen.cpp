93d813be6169b80ce6d50b9a8e8095aabc0d4a7c
Raihan Kibria <rkibria@users.noreply.github.com>
2020-06-27 21:05:51 +0100

    Example classes

--- nwnchargen.cpp
+++ nwnchargen.cpp
@@ -10,6 +10,7 @@
 #include <Nwn/ablblock.hpp>
 #include <Nwn/race.hpp>
 #include <Nwn/rules.hpp>
+#include <Nwn/chclass.hpp>
 
 #include "racedialog.h"
 #include "levelsmodel.h"
@@ -37,6 +38,14 @@ void addDebugRules( Rules* nwnRules )
 
     nwnRules->setRace( std::make_unique<Race>( "Gray Dwarf", "Dwarf" ) );
     nwnRules->setRace( std::make_unique<Race>( "Drow", "Elf" ) );
+
+    std::unique_ptr< ChClass > barbarian = std::make_unique< ChClass >( "Barbarian" );
+    barbarian->setDescription( "Barbarians are brave, even reckless warriors whose great strength and heartiness makes them well suited for adventure. Where the fighter would rely on training and discipline, the barbarian enters a berserker state that makes him stronger, tougher, and more determined but less concerned with his health. These spectacular rages leave him winded, and he only has the energy for a few a day, but those usually suffice. He also knows the wild and runs at great speed." );
+    nwnRules->setChClass( std::move( barbarian ) );
+
+    std::unique_ptr< ChClass > cleric = std::make_unique< ChClass >( "Cleric" );
+    cleric->setDescription( "Clerics are masters of divine magic, which is especially good at healing. Even an inexperienced Cleric can bring people back from the brink of death, and an experienced Cleric can bring back people who have crossed over that brink. As channelers of divine energy, clerics can affect undead creatures. A Cleric can turn away or even destroy undead. Clerics have some combat training. They can use simple weapons, and they are trained in the use of armor, since armor does not interfere with divine spells the way it does with arcane spells." );
+    nwnRules->setChClass( std::move( cleric ) );
 }
 
 void initCharacter( Character* nwnChar )
