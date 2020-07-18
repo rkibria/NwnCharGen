2cd515a6494063a63cbd4e9652e437853ceabdf0
Raihan Kibria <rkibria@users.noreply.github.com>
2020-07-18 16:56:44 +0100

    2da mapper data structs

--- rulesgen/twodamapper.cpp
+++ rulesgen/twodamapper.cpp
@@ -1,6 +1,11 @@
 #include "twodamapper.h"
 
 #include <iostream>
+#include <array>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+
 #include <unziphelper/unziphelper.h>
 
 TwoDAMapper::TwoDAMapper()
@@ -14,11 +19,27 @@ TwoDAMapper::~TwoDAMapper()
 
 void TwoDAMapper::initialize( const std::string& nwn2Path )
 {
-    const auto twoDAfiles = UnzipHelper::getFileList( ( nwn2Path + "\\Data\\2DA_X2.zip" ).c_str() );
-    for( const auto& path : twoDAfiles ) {
-        if( !path.empty() && path.back() != '/' ) {
-            std::cout << path << std::endl;
+    const std::array< std::string, 3 > twoDAzips = {
+        nwn2Path + "\\Data\\2DA.zip",
+        nwn2Path + "\\Data\\2DA_X1.zip",
+        nwn2Path + "\\Data\\2DA_X2.zip"
+    };
+
+    for( const auto& zipPath : twoDAzips ) {
+        const auto twoDAfiles = UnzipHelper::getFileList( zipPath.c_str() );
+        for( const auto& path : twoDAfiles ) {
+            if( path.empty() || path.back() == '/' ) {
+                continue;
+            }
+
+            const auto lowerPath = boost::to_lower_copy( path );
+            if( ! boost::algorithm::ends_with( lowerPath, ".2da" ) ) {
+                continue;
+            }
+
+//            std::cout << zipPath << ": " << path << std::endl;
         }
+
     }
 
 
--- rulesgen/twodamapper.h
+++ rulesgen/twodamapper.h
@@ -3,6 +3,17 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
+
+struct TwoDAMapValue
+{
+    /// Containing zip file
+    std::string zipFile;
+    /// Relative path inside containing zip file
+    std::string zipPath;
+    /// Path where extracted, empty if not yet extracted
+    std::string extractedPath;
+};
 
 class TwoDAMapper
 {
@@ -11,6 +22,9 @@ public:
     ~TwoDAMapper();
 
     void initialize( const std::string& nwn2Path );
+
+private:
+    std::unordered_map< std::string, TwoDAMapValue > extractMap;
 };
 
 #endif // TWODAMAPPER_H
